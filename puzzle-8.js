c2abfaf35b8cb2d3f05e4a010c48d9607539f39f
Jierulin <31756333+Jierulin@users.noreply.github.com>
2017-09-16 10:57:51 -0700

    Update puzzle-8.js

--- puzzle-8.js
+++ puzzle-8.js
@@ -1,22 +1,20 @@
 function action(){
-  down()
-  down()
-  down()
-  down()
-  down()
-  down()
-  down()
-  down()
+  function repeatDown(8) {
+    var count = 0;
+    while (count < 8) {
+      down();
+      count = count + 1;
+    }
+  }
   right()
   right()
-  up()
-  up()
-  up()
-  up()
-  up()
-  up()
-  up()
-  up()
+  function repeatUp(8) {
+    var count = 0;
+    while (count < 8) {
+      up();
+      count = count + 1;
+    }
+  }
   right()
   right()
 }
